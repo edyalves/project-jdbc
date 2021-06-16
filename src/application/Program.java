f4a357bfdf8f4f0d47f809c64087d9bcb8d7eab4
Edy Alves <edy4alves@gmail.com>
2021-06-16 18:06:17 -0300

    FindByDepartment implement

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,10 @@
 package application;
 
+import java.util.List;
+
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
@@ -10,9 +13,16 @@ public class Program {
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
-		Seller seller = sellerDao.findById(8);
-		
+		System.out.println("============ TEST1 =============");
+		Seller seller = sellerDao.findById(1);		
 		System.out.println(seller);
+		
+		System.out.println("============ TEST2 =============");
+		Department depart = new Department(4, null);
+		List<Seller> list = sellerDao.findByDepartment(depart);
+		for (Seller obj : list) {
+			System.out.println(obj);
+		}
 
 	}
 
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -4,7 +4,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import db.DB;
 import db.DbException;
@@ -95,6 +98,49 @@ public class SellerDaoJDBC implements SellerDao{
 	public List<Seller> findAll() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	@Override
+	public List<Seller> findByDepartment(Department dep) {
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		
+		try {
+			//DECLARO QUERY SQL
+			ps = conn.prepareStatement(
+					"SELECT seller.*,department.Name as depName "
+					+ "FROM seller "
+					+ "INNER JOIN department "
+					+ "ON seller.DepartmentId = department.Id "
+					+ "WHERE DepartmentId = ? "
+					+ "ORDER BY Name"
+			);
+			//DECLARO OS VALORES DA QUERY DO SQL
+			ps.setInt(1, dep.getId());
+			//EXECUTO QUERY COM RESULTSET
+			rs = ps.executeQuery();
+			
+			List<Seller> list = new ArrayList<Seller>();
+			Map<Integer, Department> map = new HashMap<>();
+			
+			//TRAFORMO OS DADOS EM OBJETOS
+			while(rs.next()) {
+				Department depart = map.get(rs.getInt("DepartmentId"));
+				if(depart == null) {
+					depart = instantDepartment(rs);
+					map.put(rs.getInt("DepartmentId"), depart);
+				}
+				Seller obj = instantSeller(rs, depart);
+				list.add(obj);
+			}
+			return list;
+			
+		} catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DB.closeResultSet(rs);
+			DB.closeStatement(ps);
+		}
 	}
 
 }
